1e445f38d96bd6caa4c9af723c218ab289e207f5
JPCHoogendijk <j.p.c.hoogendijk@uu.nl>
2025-03-19 17:37:11 +0100

    ApproximateSolution returns function now. Fixed expovariate argument.

--- system_of_bps.py
+++ system_of_bps.py
@@ -4,6 +4,7 @@ from collections import defaultdict, deque
 import numpy as np
 from math import exp
 import matplotlib.pyplot as plt
+from scipy.special import lambertw
 
 # Function hierarchy in the code
 # 1. Main function is ApproximateSolution, which calls GenerateTree several times
@@ -12,11 +13,11 @@ import matplotlib.pyplot as plt
 # 4. TreeFactor computes the associated value of a subtree
 # 5. BinSearch tree is a binary search in the tree that looks up the value associated to a specific point in time.
 
-def ApproximateSolution(rng, x, Ntrees, t_interval, type_dict):
+def ApproximateSolution(rng, x, Ntrees, t_cutoff, type_dict):
     # Takes:
     # - x:          an m-dimensional vector taking values in [0, \infty)^m
     # - Ntrees:     the number of trees used to approximate the solution
-    # - t_interval: numpy array denoting time
+    # - t_cutoff:   cutoff time
     # - type_dict:  defaultdict containing all types with their coefficients
 
     # Returns:
@@ -27,7 +28,6 @@ def ApproximateSolution(rng, x, Ntrees, t_interval, type_dict):
     #   - For each component generate Ntree trees, for each time point, average the trees so far
     #   - Return a sequence of vectors indexed by time
 
-    t_cutoff = t_interval[-1]
     alpha_cutoff = 1000000
     m = len(x)
 
@@ -35,20 +35,24 @@ def ApproximateSolution(rng, x, Ntrees, t_interval, type_dict):
     l1_types = GetLevelTypes(m, 1, type_dict)                   #Obtain level 1 types
     l2_types = GetLevelTypes(m, 2, type_dict)                   #Obtain level 2 types
     sol_type_dict = TypesAtX(x, l1_types, l2_types, type_dict)
-    
-    u = []
+
+    ### FOR SOME EXTRA EFFICIENCY, WE COULD REMOVE ZERO COEFFICIENTS IF TRULY NECESSARY
+    trees = []
     for k in range(1, m + 1):
-        u_k = []
         trees_k = [GenerateTree(rng, k, t_cutoff, alpha_cutoff, l1_types, l2_types, sol_type_dict) for n in range(Ntrees)]
-        for t in t_interval:
-            u_k_t = 0
-            for tree_k in trees_k:
+        trees.append(trees_k)
+
+    def ret_val(t):
+        u = []
+        for k in range(1, m + 1):
+            u_k = 0
+            for tree_k in trees[k - 1]:
                 index = BinSearchTree(t, tree_k)
-                u_k_t += tree_k[index][1]
-            u_k_t /= Ntrees
-            u_k.append(u_k_t)
-        u.append(u_k)
-    return u
+                u_k += tree_k[index][1]
+            u_k /= Ntrees
+            u.append(u_k)
+        return u
+    return ret_val
 
 def GetLevelTypes(dim, level, type_dict):
     res = [[] for _ in range(dim)]
@@ -59,6 +63,7 @@ def GetLevelTypes(dim, level, type_dict):
 
 def TypesAtX(x, l1_types, l2_types, type_dict):
     transformed_type_dict = defaultdict(float)
+    m = len(x)
     q = [1.0/len(l1k) for l1k in l1_types]
     phi = [1.0/len(l2k) for l2k in l2_types]
     for k, v in type_dict.items():
@@ -67,8 +72,13 @@ def TypesAtX(x, l1_types, l2_types, type_dict):
             dot = sum([pair[0] * pair[1] for pair in zip(k[2:], x)])
             new_coeff = v/(q[k[1] - 1]) * exp(-1.0 * dot)
         elif level == 2:
-            new_coeff = (v + 1.0)/(phi[k[1] - 1])
+            if k[2:] == tuple([0]*m):
+                new_coeff = (v + 1.0)/(phi[k[1] - 1])
+            else:
+                new_coeff = v/(phi[k[1] - 1])
+        #if new_coeff != 0.0:
         transformed_type_dict[k] = new_coeff
+
     return transformed_type_dict
 
 def GenerateTree(rng, component, t_cutoff, alpha_cutoff, l1_types, l2_types, coef):
@@ -102,13 +112,12 @@ def GenerateTree(rng, component, t_cutoff, alpha_cutoff, l1_types, l2_types, coe
         if(sum(tree_alpha) > alpha_cutoff):
             break
         #Select point in time when subtree is added
-        t += rng.expovariate(1.0/sum(tree_alpha))
+        t += rng.expovariate(sum(tree_alpha))
         if(t > t_cutoff):
             break
         #Select level 2 type from which subtree is added
         added_types = defaultdict(int)
         l2_sub_root = Samplel2SubRoot(rng, tree_alpha, l2_types)
-        added_types[l2_sub_root] += 1
         #Spawn subtree from level 2 type, increase tree alpha
         tree_alpha = GenerateSubTree(rng, tree_alpha, added_types, l2_sub_root, t, alpha_cutoff, l1_types, l2_types)
         #Compute the total tree factor from the subtree and multiply with current one
@@ -135,7 +144,7 @@ def GenerateSubTree(rng, tree_alpha, new_types, root_type, t, alpha_cutoff, l1_t
     # - rng:            the numpy random number generator
     # - tree_alpha:     a list which contains the sum of all alpha vectors in the tree 
     # - new_types:      a dictionary which tracks the number of types
-    # - time:           the time parameter
+    # - t:           the time parameter
     # - alpha_cutoff:   maximum level 1 mass in the tree
     # - l1_types:       a list of lists of tuples that contains all possible level 2 types
     #                   where list i contains all types with first index i
@@ -201,7 +210,7 @@ def GenerateLevel2FromLevel1(rng, dim, t, l1_type, l2_types):
     #               where list i contains all types with first index i.
 
     # Returns:
-    # - a numpy array of tuples, the tuples being the generated l2_types
+    # - a list of tuples, the tuples being the generated l2_types
     level = l1_type[0]
     assert level == 1, "Wrong level, input level 1 type where there should have been level 2"    
     res = []
@@ -224,18 +233,18 @@ def BinSearchTree(time, tree):
             return mid
         
         if tree[mid][0] > time:
-            right = mid - 1 
+            right = mid - 1
         elif tree[mid][0] < time:
             left = mid + 1
     #print(f"Time at right is {tree[right][0]}. Actual time is {time}") 
     return right
 
-if __name__ == "__main__":
+def test_case_1():
     rng = Random()
     rng.seed(1)
 
-    N_trees = 5120000
-    times = np.linspace(0, 0.2, 20)
+    N_trees = 10000
+    t_cutoff = 0.2
 
     # Test case 1 with the following initial condition and nonlinearity:
     # - u_1(0, x) = 0.5 e^{-x_1} + 0.5 e^{-x_2}
@@ -249,12 +258,13 @@ if __name__ == "__main__":
     t1_td[(1, 1, 0, 1)] = 0.5
     t1_td[(1, 2, 1, 0)] = -0.5
     t1_td[(1, 2, 0, 2)] = 1.5
-    t1_td[(2, 1, 1, 0)] = 1
-    t1_td[(2, 1, 0, 1)] = 1
-    t1_td[(2, 2, 1, 1)] = 1
+    t1_td[(2, 1, 1, 0)] = 1.0
+    t1_td[(2, 1, 0, 1)] = 1.0
+    t1_td[(2, 2, 1, 1)] = 1.0
     # Need to add implementation for the above conditions
     test_1_x = (1, 1)
-    test_1_u = ApproximateSolution(rng, test_1_x, N_trees, times, t1_td)
+    test_1_u = ApproximateSolution(rng, test_1_x, N_trees, t_cutoff, t1_td)
+    print(f"The resulting value at x = (1, 1), t = 0.1 is: {test_1_u(0.1)}")
 
     #fig, ax = plt.subplots()
 
@@ -280,3 +290,76 @@ if __name__ == "__main__":
     # - u_2(0, x) = -0.5 e^{-x_1} + 1.5 e^{- 2 x_2}
     # - F_1(s) = 1
     # - F_2(s) = 0
+
+def test_modified_inviscid_burgers_1d():
+    rng = Random()
+    rng.seed(1)
+
+    N_trees = 10000
+    t_cutoff = 0.95
+    t_choice = 0.90
+
+    td = defaultdict(float)
+
+    #u(0, x) = e^{-x}
+    #F(s) = s - 1
+    td[(1, 1, 1)] = 0.5
+    td[(2, 1, 1)] = 1.0
+    td[(2, 1, 0)] = -1.0
+
+    x_range = np.linspace(0, 2, 20)
+    u = []
+    u_expl = []
+    for x in x_range:
+        u_apprx = ApproximateSolution(rng, [x], N_trees, t_cutoff, td)
+        u.append(u_apprx(t_choice))
+        u_expl.append(-1.0/t_choice * lambertw(-0.5*t_choice * exp(-x-t_choice)))
+    
+    fig, ax = plt.subplots()
+
+    ax.plot(x_range, u)
+    ax.plot(x_range, u_expl)
+
+    ax.set_xlabel('x')
+    ax.set_ylabel('')
+    ax.set_title('Plot of u_t = -u u_x + u_x, u(0, x) = exp(-x)')
+    plt.show()
+
+def test_1D_wave_equation():
+    rng = Random()
+    rng.seed(2)
+
+    N_trees = 100000
+    t_cutoff = 0.9
+    t_choice = 0.8
+
+    td = defaultdict(float)
+
+    #u(0, x) = 0.5e^{-x} - 0.5 e^{-2x}
+    #F(s) = 1
+    td[(1, 1, 1)] = 0.5
+    td[(1, 1, 2)] = -0.5
+    td[(2, 1, 0)] = 1.0
+
+    x_range = np.linspace(0, 5, 20)
+    u = []
+    u_init = []
+    for x in x_range:
+        u_apprx = ApproximateSolution(rng, [x], N_trees, t_cutoff, td)
+        u.append(u_apprx(t_choice))
+        u_init.append(0.5*exp(-1.0*(x-t_choice)) - 0.5*exp(-2.0*(x - t_choice)))
+    
+    fig, ax = plt.subplots()
+
+    ax.plot(x_range, u)
+    ax.plot(x_range, u_init)
+
+    ax.set_xlabel('x')
+    ax.set_ylabel('')
+    ax.set_title('Plot of u_t = -2u_x , u(0, x) = exp(-x)')
+    plt.show()
+
+#def test_2D_wave_equation():
+
+if __name__ == "__main__":
+    test_1D_wave_equation()
